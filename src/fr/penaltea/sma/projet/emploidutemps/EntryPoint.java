77ffa7978cc4f9d148fbf1255f57378517e497dd
Benoît Sauvère <benoit.sauvere@teevity.com>
2016-03-24 11:54:18 +0100

    Ajout du sujet dans les resources

--- src/fr/penaltea/sma/projet/emploidutemps/EntryPoint.java
+++ src/fr/penaltea/sma/projet/emploidutemps/EntryPoint.java
@@ -1,10 +1,13 @@
 package fr.penaltea.sma.projet.emploidutemps;
 
-
+/**
+ * 
+ * @author Benoît Sauvère
+ *
+ */
 public class EntryPoint {
 
 	public static void main(String[] args) {
-
+			
 	}
-
 }
